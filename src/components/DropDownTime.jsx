1dc6e2909b5e5d28aff63bb0e9dcff65be5f73c8
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-10-03 16:47:37 -0300

    Add note in dropdown time

--- src/components/DropDownTime.jsx
+++ src/components/DropDownTime.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { FaInfoCircle } from "react-icons/fa";
+
 export default function DropDownTime({
   forecastTime,
   setForecastTime,
@@ -18,13 +21,51 @@ export default function DropDownTime({
     setFrame({ ...frame, forecastTime: time });
   };
 
+  const [showNote, setShowNote] = useState(false);
+
+  const handleShowNote = () => {
+    setShowNote(!showNote);
+  };
+
   return (
     <>
       <div className="absolute top-12 right-0 bg-white border border-gray-200 rounded-md w-60 z-50">
         <div className="p-4">
           <label className="block w-full pb-3 text-sm font-bold">
             Horas de previsão
+            <sup className="text-red-600">
+              <FaInfoCircle className="inline" />
+            </sup>
           </label>
+          <div className="text-xs bg-red-50 p-2 border border-red-100 rounded-md mb-3">
+            <b>Nota importante:</b> A previsão da qualidade do ar começa no dia
+            anterior,{" "}
+            {!showNote && (
+              <>
+                <span
+                  onClick={() => setShowNote(true)}
+                  className="cursor-pointer text-red-600"
+                >
+                  [continuar lendo...]
+                </span>
+              </>
+            )}
+            {showNote && (
+              <>
+                com dados de emissões de queimadas, baseados nos focos
+                detectados por satélites (análise, indicada por horários
+                negativos). Em seguida, é feita a previsão para as 72 horas
+                seguintes, assumindo que as emissões estimadas na análise
+                permanecerão as mesmas.{" "}
+                <span
+                  onClick={() => setShowNote(false)}
+                  className="cursor-pointer text-red-600"
+                >
+                  [ocultar nota]
+                </span>
+              </>
+            )}
+          </div>
           <div className="flex flex-wrap gap-1">
             {hours.map((time, index) => (
               <button
