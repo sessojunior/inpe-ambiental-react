6fdf780364b085421b39927e606dec21b52c1d0b
Mario Antonio Sesso Junior <sessojunior@gmail.com>
2024-12-30 13:27:57 -0300

    Updated time refresh after updated app

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -42,8 +42,8 @@ export default function Frame({ id }) {
 
     return (
       <div className="p-8 text-red-500">
-        Foi feita uma atualização no aplicativo. Recarregue a página ou aguarde
-        5 segundos para recarregamento automático.
+        Foi feita uma atualização no aplicativo. Aguarde 5 segundos para
+        recarregamento automático.
       </div>
     );
   }
